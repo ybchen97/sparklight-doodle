1b8aaf7c1b1c1dccf5a9af5737430c03544f9650
ysong442 <ysong442@gatech.edu>
2023-11-11 19:53:45 -0800

    working detection of touching palm

---
+++ Assets/Scripts/TrackingLogic/HandPoseClassUtils.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class DetectedHand
+{
+    Hand? _lastDetected;
+    Hand? _current;
+    public Hand? LastDetected { get => _lastDetected; set => _lastDetected = value; }
+    public Hand? Current { get => _current; set => _current = value; }
+}
+
+public enum DrawState
+{
+    EMPTY,
+    DRAWING,
+    DRAWING_FINISHED,
+    SENDING,
+    COOLING_DOWN,
+}
+
